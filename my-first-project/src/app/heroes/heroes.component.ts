d57ab2a5f1bd67ac88ba4e301643982eaf4148f2
Johannes Danusantoso <jdanusantoso@hotmail.com>
2023-02-27 15:29:41 -0600

    added a hero

--- my-first-project/src/app/heroes/heroes.component.ts
+++ my-first-project/src/app/heroes/heroes.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-heroes',
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
+
+  hero = "Windstorm";
+
+  constructor() {}
+
+  ngOnInit (){
+
+  }
 
 }
