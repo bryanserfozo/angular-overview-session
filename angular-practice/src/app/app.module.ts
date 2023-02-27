c5ff2b1e9581872cb7423b7283e7d7cec6804240
Ryan Sullivan <mrsulliv@gmail.com>
2023-02-27 13:46:05 -0800

    hitting geodb api and just logging results for now

--- angular-practice/src/app/app.module.ts
+++ angular-practice/src/app/app.module.ts
@@ -2,21 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { CitiesComponent } from './cities/cities.component';
 import { CountriesComponent } from './countries/countries.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CitiesComponent,
-    CountriesComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CitiesComponent, CountriesComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- angular-practice/src/app/cities/cities.component.ts
+++ angular-practice/src/app/cities/cities.component.ts
@@ -1,9 +1,35 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-cities',
   templateUrl: './cities.component.html',
   styleUrls: ['./cities.component.sass'],
 })
-export class CitiesComponent {}
+export class CitiesComponent {
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'application/json')
+      .set(
+        'X-RapidAPI-Key',
+        'fbd4193a19msh070d214481500e1p1b517cjsnc35b709e7001'
+      )
+      .set('X-RapidAPI-Host', 'wft-geo-db.p.rapidapi.com');
+
+    this.http
+      .get<any>(`https://wft-geo-db.p.rapidapi.com/v1/geo/cities`, {
+        headers,
+      })
+      .pipe(
+        catchError((e) => {
+          return throwError(e);
+        })
+      )
+      .subscribe((data) => {
+        console.log(data);
+      });
+  }
+}
